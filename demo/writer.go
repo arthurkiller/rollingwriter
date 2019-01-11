956e0b6f74dcc63e81b8cb28d945e8d31283075e
arthur <arthurkiller21@gmail.com>
2019-01-11 15:42:27 +0800

    fix demo

--- demo/writer.go
+++ demo/writer.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"crypto/rand"
+	"fmt"
 	"sync"
+	"time"
 
 	"github.com/arthurkiller/rollingWriter"
 )
@@ -19,17 +20,21 @@ func main() {
 		// 目前有2中滚动策略: 按照时间滚动按照大小滚动
 		// - 时间滚动: 配置策略如同 crontable, 例如,每天0:0切分, 则配置 0 0 0 * * *
 		// - 大小滚动: 配置单个日志文件(未压缩)的滚动大小门限, 入1G, 500M
-		RollingPolicy:      rollingwriter.TimeRolling, //配置滚动策略 norolling timerolling volumerolling
-		RollingTimePattern: "* * * * * *",             //配置时间滚动策略
-		RollingVolumeSize:  "500M",                    //配置截断文件下限大小
-		Compress:           true,                      //配置是否压缩存储
+		RollingPolicy:      rollingwriter.VolumeRolling, //配置滚动策略 norolling timerolling volumerolling
+		RollingTimePattern: "* * * * * *",               //配置时间滚动策略
+		RollingVolumeSize:  "2k",                        //配置截断文件下限大小
 
-		// writer 支持3种方式:
+		// writer 支持4种不同的 mode:
+		// 1. none 2. lock
+		// 3. async 4. buffer
 		// - 无保护的 writer: 不提供并发安全保障
 		// - lock 保护的 writer: 提供由 mutex 保护的并发安全保障
 		// - 异步 writer: 异步 write, 并发安全. 异步开启后忽略 Lock 选项
-		Asynchronous: false, //配置是否异步写
-		Lock:         true,  //配置是否同步加锁写
+		WriterMode: "lock",
+		// BufferWriterThershould in MB
+		BufferWriterThershould: 4,
+		// Compress will compress log file with gzip
+		Compress: true,
 	}
 
 	// 创建一个 writer
@@ -42,15 +47,14 @@ func main() {
 	// 并发读写即可
 	wg := sync.WaitGroup{}
 	bf := make([]byte, 128)
-	rand.Read(bf)
-	for i := 0; i < 50; i++ {
+	for i := 0; i < 1; i++ {
 		wg.Add(1)
 		go func() {
 			for {
+				fmt.Fprintf(writer, "now :%s \n", time.Now())
 				writer.Write(bf)
 			}
 		}()
 	}
-
 	wg.Wait()
 }
