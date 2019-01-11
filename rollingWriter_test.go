e55607746956402af65ea6d0892ce7b159398ebf
arthur <arthurkiller21@gmail.com>
2019-01-11 21:13:25 +0800

    add unit test

---
+++ rollingWriter_test.go
@@ -0,0 +1,33 @@
+package rollingwriter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOptions(t *testing.T) {
+	options := []Option{
+		WithTimeTagFormat("200601021504"), WithLogPath("./"), WithFileName("foo"),
+		WithAsynchronous(), WithBuffer(), WithBufferThershould(8), WithCompress(), WithLock(),
+		WithMaxRemain(3), WithRollingVolumeSize("100mb"), WithRollingTimePattern("0 0 0 * * *"),
+	}
+	cfg := NewDefaultConfig()
+	for _, opt := range options {
+		opt(&cfg)
+	}
+
+	destcfg := Config{
+		LogPath:                "./",
+		TimeTagFormat:          "200601021504",
+		FileName:               "foo",
+		MaxRemain:              3,             // disable auto delete
+		RollingPolicy:          2,             // TimeRotate by default
+		RollingTimePattern:     "0 0 0 * * *", // Rolling at 00:00 AM everyday
+		RollingVolumeSize:      "100mb",
+		WriterMode:             "lock",
+		BufferWriterThershould: 8,
+		Compress:               true,
+	}
+	assert.Equal(t, cfg, destcfg)
+}
